5d3b3867a923f36b37ed715f08bc9854b1d7acf5
Matt Basta <matt@runwayml.com>
2025-10-09 14:18:42 -0400

    Make sound effects waitable

--- src/runwayml/resources/sound_effect.py
+++ src/runwayml/resources/sound_effect.py
@@ -17,6 +17,12 @@ from .._response import (
     async_to_raw_response_wrapper,
     async_to_streamed_response_wrapper,
 )
+from ..lib.polling import (
+    NewTaskCreatedResponse,
+    AsyncNewTaskCreatedResponse,
+    create_waitable_resource,
+    create_async_waitable_resource,
+)
 from .._base_client import make_request_options
 from ..types.sound_effect_create_response import SoundEffectCreateResponse
 
@@ -56,7 +62,7 @@ class SoundEffectResource(SyncAPIResource):
         extra_query: Query | None = None,
         extra_body: Body | None = None,
         timeout: float | httpx.Timeout | None | NotGiven = not_given,
-    ) -> SoundEffectCreateResponse:
+    ) -> NewTaskCreatedResponse:
         """
         This endpoint will start a new task to generate sound effects from a text
         description.
@@ -94,7 +100,7 @@ class SoundEffectResource(SyncAPIResource):
             options=make_request_options(
                 extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
             ),
-            cast_to=SoundEffectCreateResponse,
+            cast_to=create_waitable_resource(SoundEffectCreateResponse, self._client),
         )
 
 
@@ -131,7 +137,7 @@ class AsyncSoundEffectResource(AsyncAPIResource):
         extra_query: Query | None = None,
         extra_body: Body | None = None,
         timeout: float | httpx.Timeout | None | NotGiven = not_given,
-    ) -> SoundEffectCreateResponse:
+    ) -> AsyncNewTaskCreatedResponse:
         """
         This endpoint will start a new task to generate sound effects from a text
         description.
@@ -169,7 +175,7 @@ class AsyncSoundEffectResource(AsyncAPIResource):
             options=make_request_options(
                 extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
             ),
-            cast_to=SoundEffectCreateResponse,
+            cast_to=create_async_waitable_resource(SoundEffectCreateResponse, self._client),
         )
 
 
