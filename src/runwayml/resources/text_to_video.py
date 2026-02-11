f024d8e35ca7687ac7e5815e2e7bfbf784316e21
Matt Basta <matt@runwayml.com>
2026-02-11 11:45:10 -0500

    fix(client): Update generated types for 4.5 t2v

--- src/runwayml/resources/text_to_video.py
+++ src/runwayml/resources/text_to_video.py
@@ -65,7 +65,7 @@ class TextToVideoResource(SyncAPIResource):
         extra_query: Query | None = None,
         extra_body: Body | None = None,
         timeout: float | httpx.Timeout | None | NotGiven = not_given,
-    ) -> TextToVideoCreateResponse:
+    ) -> NewTaskCreatedResponse:
         """
         This endpoint will start a new task to generate a video from a text prompt.
 
@@ -283,7 +283,7 @@ class AsyncTextToVideoResource(AsyncAPIResource):
         extra_query: Query | None = None,
         extra_body: Body | None = None,
         timeout: float | httpx.Timeout | None | NotGiven = not_given,
-    ) -> TextToVideoCreateResponse:
+    ) -> AsyncNewTaskCreatedResponse:
         """
         This endpoint will start a new task to generate a video from a text prompt.
 
